015cc2a6fa1aa9511fbf251036e9a46e18e3f2dd
AnnaAkimova <aak@sitecore.net>
2018-08-15 15:06:20 +0300

    Init commit

---
+++ src/Sitecore.Support.219986/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.219986")]
+[assembly: AssemblyProduct("Sitecore.Support.219986")]
+[assembly: ComVisible(false)]
