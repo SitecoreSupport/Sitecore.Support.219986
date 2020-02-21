612abb4da7cdf01f6a0cc2cefe9e86c4b34a820c
sc-AndriyShcherbak <ands@sitecore.net>
2020-02-21 09:24:47 +0200

    Fix

--- src/Sitecore.Support.219986/sitecore/shell/client/Speak/Layouts/Renderings/Data/DataProviders/GenericDataProvider.js
+++ src/Sitecore.Support.219986/sitecore/shell/client/Speak/Layouts/Renderings/Data/DataProviders/GenericDataProvider.js
@@ -134,7 +134,8 @@ define(["sitecore", "baseDataprovider"], function (_sc) {
       this.model.set("hasMoreData", jsonData.hasMoreData);
         // THe fix for 219986 bug: display current records count instead of the total one
       var dataSet = jsonData.data[Object.keys(jsonData.data)[0]];
-      this.model.set("totalRecordCount", dataSet[Object.keys(dataSet)[0]].length);	 
+      this.model.set("totalRecordCount", dataSet[Object.keys(dataSet)[0]].length);
+      // the end of the changes  
       this.model.set("pageSize", jsonData.pageSize);
       this.model.set("pageNumber", jsonData.pageNumber);
 
