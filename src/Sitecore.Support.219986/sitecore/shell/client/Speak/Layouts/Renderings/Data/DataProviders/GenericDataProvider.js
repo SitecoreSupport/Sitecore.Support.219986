64fabc98b536c94a76df7a37e0ecca38ca0b0941
Sergey Plashenko <sergey.plashenko@sitecore.com>
2020-09-24 18:12:42 +0300

    data property should be checked before accessing.

--- src/Sitecore.Support.219986/sitecore/shell/client/Speak/Layouts/Renderings/Data/DataProviders/GenericDataProvider.js
+++ src/Sitecore.Support.219986/sitecore/shell/client/Speak/Layouts/Renderings/Data/DataProviders/GenericDataProvider.js
@@ -115,10 +115,17 @@ define(["sitecore", "baseDataprovider"], function (_sc) {
 
     successHandler: function (jsonData) {      
       
-        this.model.set("hasMoreData", jsonData.hasMoreData);
-        // THe fix for 219986 bug: display current records count instead of the total one
-      var dataSet = jsonData.data[Object.keys(jsonData.data)[0]];
-      this.model.set("totalRecordCount", dataSet[Object.keys(dataSet)[0]].length);	  
+      this.model.set("hasMoreData", jsonData.hasMoreData);
+      
+      // The fix for 219986 bug: display current records count instead of the total one
+      if (jsonData.data){
+        var dataSet = jsonData.data[Object.keys(jsonData.data)[0]];
+        this.model.set("totalRecordCount", dataSet[Object.keys(dataSet)[0]].length);	  
+      }
+      else{
+        this.model.set("totalRecordCount", jsonData.totalRecordCount);
+      }
+
       this.model.set("pageSize", jsonData.pageSize);
       this.model.set("pageNumber", jsonData.pageNumber);
 
